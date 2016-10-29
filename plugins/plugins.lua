4a33db0775e6ecd1979f7da197d96b36c8a734f2
MoonTeamDev <makan.j130130@gmail.com>
2016-10-30 00:28:02 +0330

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -27,13 +27,13 @@ local function list_all_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ✔ enabled, ❌ disabled
-    local status = '/Disable➣'
+    local status = '🔴'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '/Enable➣' 
+        status = '🔵' 
       end
       nact = nact+1
     end
@@ -52,24 +52,24 @@ local function list_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ✔ enabled, ❌ disabled
-    local status = '/Disable➣'
+    local status = '🔴'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '/Enable➣' 
+        status = '🔵' 
       end
       nact = nact+1
     end
-    if not only_enabled or status == '/Enable➣' then
+    if not only_enabled or status == '🔵' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
      -- text = text..v..'  '..status..'\n'
     end
   end
-  local text = text.."\nAll Plugins Reloaded\n\n"..nact.." Plugins Enabled\n"..nsum.." Plugins Installed\n\n@MoonsTeam"
-return text
+  local text = text..'\nPlugins Reloaded !\n\n'..nact..' plugins enabled\n'..nsum..' plugins installed\n\n@MoonsTeam'
+  return text
 end
 
 local function reload_plugins( )
@@ -222,7 +222,7 @@ return {
     "^!pl? (+) ([%w_%.%-]+) (chat)",
     "^!pl? (-) ([%w_%.%-]+) (chat)",
     "^!pl? (*)$",
-    --"^[!/](reload)$"
+   --"^[!/](reload)$"
     },
   run = run,
   moderated = true, -- set to moderator mode
