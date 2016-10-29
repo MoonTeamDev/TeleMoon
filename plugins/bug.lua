30354282a6eb6d454044efbe16aab6cb2971c8e5
MoonTeamDev <makan.j130130@gmail.com>
2016-10-29 23:29:31 +0330

    Create bug.lua

---
+++ plugins/bug.lua
@@ -0,0 +1,13 @@
+local function run(msg,matches)
+  local m = matches[1]
+  local bug = io.popen( 'lua ./plugins/'..m..'.lua' ):read('*all')
+  return bug
+  end
+  
+return {
+    patterns={
+      '^[/#!][Bb][Uu][Gg] (.*)$',
+      '^[Bb][Uu][Gg] (.*)$',	  
+    },
+    run = run
+  }
