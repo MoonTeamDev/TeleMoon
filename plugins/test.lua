2878d3df5bbe196c7bb2698dd7cbb18cf1fb6dc7
MoonTeamDev <makan.j130130@gmail.com>
2016-10-31 17:57:18 +0330

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -2375,7 +2375,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Message /superhelp to @Teleseed in private for SuperGroup help"
+			text = "Owner's only"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
