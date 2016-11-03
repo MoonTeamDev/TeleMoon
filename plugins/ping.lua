d42de7f9999ffe63fe67de8f6f9222ae2d86bf2b
MoonTeamDev <makan.j130130@gmail.com>
2016-11-03 20:13:24 +0330

    Update ping.lua

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -1,14 +1,35 @@
-local datebase = {
-  "Im here😒",
-  "What the hell😡",
-  "Online :/",
-  }
-local function run(msg, matches)
-return datebase[math.random(#datebase)]
+do
+
+function run(msg, matches)
+if msg.from.id == 90285047 then
+  return "<i>I'm online</i> <a href="telegram.me/makan">Dadi 😍😘</a>"
+elseif is_sudo(msg) then
+ return "<i>I am online</i> <b>fuck you</b>😐"
+elseif is_admin1(msg) then
+ return "<b>fuck you</b>😐\n<i>I am online</i>͡° ͜ʖ ͡°"
+elseif is_owner(msg) then
+  return "<b>I'm online </b>😉"
+elseif is_momod(msg) then
+ return "<b>I'm online bitch :|️</b>"
+else 
+return "<b>I'm here mother fucker  
+       /🖕
+ (°·°)/
+ / | /
+/  | 
+ \ |8=====D
+  / \
+ /   \</b>"
+end
 end
 return {
+  description = "", 
+  usage = "",
   patterns = {
-    "^[Pp][Ii][Nn][Gg]",
+    "^[!/#](bot)$",
+    "^[!/#](ping)$",
+    "^[!/#](tik)$",
   },
   run = run
 }
+end
