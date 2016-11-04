8015defb36dfb236643d36d263199eba218c7701
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 02:16:10 +0330

    Update topng.lua

--- plugins/topng.lua
+++ plugins/topng.lua
@@ -1,12 +1,12 @@
-local function topng(msg, success, result) 
+local function toimage(msg, success, result) 
   local receiver = get_receiver(msg) 
   if success then 
-    local file = './data/topng/'..msg.from.id..'.png' 
+    local file = './data/tophoto/'..msg.from.id..'.jpg' 
     print('File downloaded to:', result) 
     os.rename(result, file) 
     print('File moved to:', file) 
-    send_document(get_receiver(msg), file, ok_cb, false) 
-    redis:del("photo:png") 
+    send_photo(get_receiver(msg), file, ok_cb, false) 
+    redis:del("sticker:photo") 
   else 
     print('Error downloading: '..msg.id) 
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
@@ -16,7 +16,7 @@ end
 return {
    
      patterns = {
-   "^[/!#](png)$",
+   "^[/!#](photo)$",
 },
 run = run,
 }
