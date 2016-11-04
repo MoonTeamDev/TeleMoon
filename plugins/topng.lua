388fabfc213660535c290d818eb7120c66a441e0
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 02:23:27 +0330

    Update topng.lua

--- plugins/topng.lua
+++ plugins/topng.lua
@@ -1,22 +1,21 @@
-local function toimage(msg, success, result) 
-  local receiver = get_receiver(msg) 
-  if success then 
-    local file = './data/tophoto/'..msg.from.id..'.jpg' 
-    print('File downloaded to:', result) 
-    os.rename(result, file) 
-    print('File moved to:', file) 
-    send_photo(get_receiver(msg), file, ok_cb, false) 
-    redis:del("sticker:photo") 
-  else 
-    print('Error downloading: '..msg.id) 
-    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
-  end 
-end 
+local receiver = get_receiver(msg) 
+    local group = msg.to.id 
+    if msg.reply_id then 
+       if msg.to.type == 'document' and redis:get("gif:video") then 
+        if redis:set("gif:video", "waiting") then 
+        end 
+       end 
+      if matches[1]:lower() == "video"  then 
+     redis:get("gif:video") 
+    send_large_msg(receiver, '', ok_cb, false) 
+        load_document(msg.reply_id, tovideo, msg) 
+    end 
+end  
 
 return {
    
      patterns = {
-   "^[/!#](photo)$",
+   "^[/!#](gif)$",
 },
 run = run,
 }
