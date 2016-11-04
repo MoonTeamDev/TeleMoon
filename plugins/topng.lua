df1496346fc65fac8bda12c7c99f76d80c42ef10
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 02:41:24 +0330

    Update and rename fun.lua to topng.lua

---
+++ plugins/topng.lua
@@ -0,0 +1,20 @@
+local function topng(msg, success, result) 
+  local receiver = get_receiver(msg) 
+  if success then 
+    local file = './data/topng/'..msg.from.id..'.png' 
+    print('File downloaded to:', result) 
+    os.rename(result, file) 
+    print('File moved to:', file) 
+    send_document(get_receiver(msg), file, ok_cb, false) 
+    redis:del("photo:png") 
+  else 
+    print('Error downloading: '..msg.id) 
+    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
+  end 
+end 
+ return {
+  patterns = {
+"^[/!#](png)
+ }, 
+run = run, 
+} 
