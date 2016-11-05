427500b76170d1ef2d85a7f30f8ecf1d6403779c
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 04:50:48 +0330

    Update timesticker.lua

--- plugins/timesticker.lua
+++ plugins/timesticker.lua
@@ -1,14 +1,26 @@
-local function run(msg, matches)
-local url , res = http.request('http://api.gpmod.ir/time/')
-local jdat = json:decode(url)
-local url = "http://latex.codecogs.com/png.download?".."\\dpi{500}%10\\LARGE%75"..jdat.ENtime
-local file = download_to_file(url,'signal.webp')
-send_document(get_receiver(msg) , file, ok_cb, false)
-end
+do
+function run(msg, matches) 
  
-return {
-  patterns = {
-    "^[!/#][Tt][Ii][Mm][Ee]$"
+      
+      local url , res = http.request('http://api.gpmod.ir/time/')
+
+      local jdat = json:decode(url)
+      local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..jdat.ENtime
+       local  file = download_to_file(url,'emoji.webp') 
+         send_document(get_receiver(msg), file, ok_cb, false) 
+
+end 
+end
+
+return { 
+  description = "time", 
+  usage = { 
+    "time نشان دادن ساعت", 
+  }, 
+  patterns = { 
+   "^[Tt]ime$", 
+   "^[!#/][Tt]ime$",
+   "ساعت",   
   }, 
   run = run 
-}
+} 
