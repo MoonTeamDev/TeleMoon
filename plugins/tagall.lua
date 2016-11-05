c08babe8aca891275d085645dc0cf2842fa5fd18
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 05:08:30 +0330

    Update tagall.lua

--- plugins/tagall.lua
+++ plugins/tagall.lua
@@ -1,26 +1,46 @@
---Tag ppl with username and a msg after it
 local function tagall(cb_extra, success, result)
     local receiver = cb_extra.receiver
-    local channel_id = "channel#id"..result.id
     local text = ''
-    local i = 0 + 1
+local msgss = 0
     for k,v in pairs(result.members) do
+ 
         if v.username then
-			text = text..i.."👤 @"..v.username.."\n"
-		end
+  
+       msgss = msgss + 1
+   text = text..msgss.."-  @"..v.username.."\n"
+  end
     end
-	text = text.."\n"..cb_extra.msg_text
-	send_large_msg(receiver, text)
+ text = text.."\n"..cb_extra.msg_text
+ send_large_msg(receiver, text)
+end
+local function tagall2(cb_extra, success, result)
+    local receiver = cb_extra.receiver
+    local text = ''
+local msgss = 0
+    for k,v in pairs(result) do
+ 
+        if v.username then
+  
+       msgss = msgss + 1
+   text = text..msgss.."-  @"..v.username.."\n"
+  end
+    end
+ text = text.."\n"..cb_extra.msg_text
+ send_large_msg(receiver, text)
 end
 local function run(msg, matches)
     local receiver = get_receiver(msg)
-	if not is_momod(msg) then 
-		return "For moderators only !"
-	end
-	if matches[1] then
-		chat_info(receiver, tagall, {receiver = receiver,msg_text = matches[1]})
-	end
-	return
+ if not is_owner(msg) then 
+  return "شما صاحب گروه نیستید!"
+ end
+ if matches[1] then
+ if msg.to.type == 'chat' then
+ chat_info(receiver, tagall, {receiver = receiver,msg_text = matches[1]})
+ elseif msg.to.type == 'channel' then
+ channel_get_users(receiver, tagall2, {receiver = receiver,msg_text = matches[1]})
+ end
+ end
+ return
 end
 
 
