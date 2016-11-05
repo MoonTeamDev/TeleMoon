b841a40f7d6bce5257f23531fbb840f2aa2bfd39
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 21:32:47 +0330

    Update sgif.lua

--- plugins/sgif.lua
+++ plugins/sgif.lua
@@ -6,11 +6,11 @@ for text in matches[3]:gmatch("[^\r\n]+") do
 texts[#texts + 1] = URL.escape(text)
 end
 if #texts == 0 then
-send_message(msg.to.peer_id, '❌ متن ها تشخیص داده نشد لطفا هر متن را در یک سطر وارد نمایید',ok_cb,false)
+send_message(get_receiver(msg), '❌ متن ها تشخیص داده نشد لطفا هر متن را در یک سطر وارد نمایید',ok_cb,false)
 return
 end
-local sgif = download_to_file('http://magic-team.ir/wm.php?wtext='..URL.escape(matches[2])..'&texts='..json:encode(texts), 'sgif.gif')
-send_document(msg.to.peer_id, sgif,ok_cb,false)
+local sgif = download_to_file('http://pars.ara-parsa.ir/gif/wm.php?wtext='..URL.escape(matches[2])..'&texts='..json:encode(texts), 'sgif.gif')
+send_document(get_receiver(msg) , sgif,ok_cb,false)
 
 else
 local texts = {}
@@ -18,15 +18,11 @@ for text in matches[2]:gmatch("[^\r\n]+") do
 texts[#texts + 1] = URL.escape(text)
 end
 if #texts == 0 then
-send_message(msg.to.peer_id, '❌ متن ها تشخیص داده نشد لطفا هر متن را در یک سطر وارد نمایید',ok_cb,false)
+send_message(get_receiver(msg) , '❌ متن ها تشخیص داده نشد لطفا هر متن را در یک سطر وارد نمایید',ok_cb,false)
 return
 end
-local sgif = download_to_file('http://magic-team.ir/wm.php?texts='..json:encode(texts), 'sgif.gif')
-if msg.to.peer_type == 'user' then
-send_document(msg.from.peer_id, sgif,ok_cb,false)
-else
-send_document(msg.to.peer_id, sgif,ok_cb,false)
-end
+local sgif = download_to_file('http://pars.ara-parsa.ir/gif/wm.php?wtext='..json:encode(texts), 'sgif.gif')
+send_document(get_receiver(msg), sgif,ok_cb,false)
 end
 end
 return {
