cdceec841579454e3e61022ca7c3e9cd55be0974
MoonTeamDev <makan.j130130@gmail.com>
2016-11-05 21:17:08 +0330

    Update myinfo.lua

--- plugins/myinfo.lua
+++ plugins/myinfo.lua
@@ -1,15 +1,13 @@
 do
 function run(msg, matches)
-local reply_id = msg['id']
-local text = 'myinfo'
-local text1 =""..(msg.from.print_name or '')\n"..(msg.from.username or '')\n"..(msg.from.id or '')
-  reply_msg(reply_id, text1, ok_cb, false)
+  return "Group id: "..msg.from.id.."\nGroup name: "..msg.to.title.."\nFirst name: "..(msg.from.first_name or '').."\nLast name: "..(msg.from.last_name or '').."\nYour id: "..msg.from.id.."\nYour username: @"..(msg.from.username or '').."\nPhone: +"..(msg.from.phone or '')
 end
 return {
-  description = "",
+  description = "", 
   usage = "",
   patterns = {
     "^[!/#]myinfo$",
+    "^(اطلاعات)$"
   },
   run = run
 }
