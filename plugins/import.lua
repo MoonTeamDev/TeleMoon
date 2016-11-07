c4d709e3308624d26608497caa5ba3f876e73946
MoonTeamDev <makan.j130130@gmail.com>
2016-11-07 18:11:33 +0330

    Update import.lua

--- plugins/import.lua
+++ plugins/import.lua
@@ -5,19 +5,15 @@ local function parsed_url(link)
 end
 
 local function run(msg,matches)
-   local creed = "user#id"..239350998
+   local telemoon = "user#id"..140979189
   if msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(our_id) then
    local texi_add = 'Now '..string.gsub(msg.from.print_name, '_', ' ')..' ['..msg.from.id..'] #Added Me to '..string.gsub(msg.to.print_name, '_', ' ')..' ['..msg.to.id..']\n#Check_Now'
- send_msg(creed, texi_add, ok_cb, false)
+ send_msg(telemoon, texi_add, ok_cb, false)
  end
-  if msg.service and msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(our_id) then
-   local texi_del = 'Now '..string.gsub(msg.from.print_name, '_', ' ')..' ['..msg.from.id..'] #Removed Me From '..string.gsub(msg.to.print_name, '_', ' ')..' ['..msg.to.id..']\n#Check_Now'
-  send_msg(creed, texi_del, ok_cb, false)
- end
-  if matches[1] == "import" and is_admin(msg) then --join by group link
+  if matches[1] == "import" then --join by group link
    local hash = parsed_url(matches[2])
    local texi = string.gsub(msg.from.print_name, '_', ' ')..' Invited Me By This LinkðŸ‘‡\n'..matches[2]
-  send_msg(creed, texi, ok_cb, false)
+  send_msg(telemoon, texi, ok_cb, false)
    import_chat_link(hash,ok_cb,false)
   end
  end
