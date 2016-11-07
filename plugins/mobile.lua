38c1b55dc7944bda0245199b228e7299d9a6c83a
MoonTeamDev <makan.j130130@gmail.com>
2016-11-08 00:51:58 +0330

    Update mobile.lua

--- plugins/mobile.lua
+++ plugins/mobile.lua
@@ -1,19 +1,14 @@
+local pass = 'dram1135'
 local function run(msg, matches)
-local url = http.request("http://www.mobile.ir/phones/AjaxLatestPhonesJson.aspx")
-  local jdat = json:decode(url)
-  local text2 = ''
-  for i = 1 , #jdat do
-    text2 = text2..i
-    text2 = text2..' ) mobile name : '..jdat[i].title
-	text2 = text2..'\ndescription : http://www.mobile.ir'..jdat[i].url.."\n"
-    end
-  return reply_msg(msg.id, text2, ok_cb, false)
+  local url = 'http://api.avirateam.ir/mobile/cli/index.php?pass='..pass
+  local req = http.request(url)
+  return req..' @MoonsTeam'
 end
 
 return {
   description = "Get New Mobile Phones From Mobile.ir",
   patterns = {
-    "^[/!#]([Mm][Oo][Bb][Ii][Ll][Ee])$"
+    "^[!/#]([Mm][Oo][Bb][Ii][Ll][Ee])$"
   },
   run = run
 }
