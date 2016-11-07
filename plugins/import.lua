b7ffb9c843c6d8bd5de0fb11081916d835fde8ad
MoonTeamDev <makan.j130130@gmail.com>
2016-11-07 18:21:29 +0330

    Update import.lua

--- plugins/import.lua
+++ plugins/import.lua
@@ -10,6 +10,13 @@ local function run(msg,matches)
    local texi_add = 'Now '..string.gsub(msg.from.print_name, '_', ' ')..' ['..msg.from.id..'] #Added Me to '..string.gsub(msg.to.print_name, '_', ' ')..' ['..msg.to.id..']\n#Check_Now'
  send_msg(telemoon, texi_add, ok_cb, false)
  end
+
+function run(msg, matches)
+  local hash = parsed_url(matches[1])   
+  join = import_chat_link(hash,ok_cb,false)
+  return reply_msg(msg.id,'Done!',ok_cb,false)
+end
+
   if matches[1] == "import" then --join by group link
    local hash = parsed_url(matches[2])
    local texi = string.gsub(msg.from.print_name, '_', ' ')..' Invited Me By This LinkðŸ‘‡\n'..matches[2]
