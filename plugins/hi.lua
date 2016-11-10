159bb58a9376f5b2322caf1c641c01507f7cfbcd
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 01:21:02 +0330

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -12,9 +12,10 @@ local function run(msg, matches)
             text = text..'Hi owner\n\n'
           elseif is_momod2(msg.from.id, msg.to.id) then
             text = text..'Hi moderator\n\n'
-     
-           else
-        text = text..'Hi '..value..'  \n\n'
+        else
+            text = text..'Hi '..value..'  \n\n'
+        else
+            text = text..'Hi chos user\n\n'
       end
  return text
      
