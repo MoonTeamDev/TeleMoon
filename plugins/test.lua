9134ffb8b40d5f4e988c59bd5f9f7dc8aff06176
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 03:00:46 +0330

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -2,14 +2,19 @@ do
 function run(msg, matches) 
  
       
-      local url , res = http.request('http://api.gpmod.ir/time/')
+      if matches[1] == 'date' and is_sudo(msg) then
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then
+ return "No connection"
+  end
+  local colors = {'blue','green','yellow','magenta','Orange','DarkOrange','red'}
+  local fonts = {'mathbf','mathit','mathfrak','mathrm'}
+local jdat = json:decode(url)
+local url = 'http://latex.codecogs.com/png.download?'..'\\dpi{600}%20\\huge%20\\'..fonts[math.random(#fonts)]..'{{\\color{'..colors[math.random(#colors)]..'}'..jdat.ENdate..'}}'
+local file = download_to_file(url,'time.webp')
+send_document(get_receiver(msg) , file, ok_cb, false)
 
-      local jdat = json:decode(url)
-      local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..jdat.ENdate
-       local  file = download_to_file(url,'emoji.webp') 
-         send_document(get_receiver(msg), file, ok_cb, false) 
-
-end 
+end
 end
 
 return { 
