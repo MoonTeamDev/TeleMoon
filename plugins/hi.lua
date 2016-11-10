79e5953b7a56328092b6f4518baf2add1752f7aa
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 01:57:29 +0330

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -1,32 +1,17 @@
-local function run(msg, matches)
- local makan = 90285047
-   local hash = 'rank:variables'
-   local text = 'Hi '..value..' '
-     local value = redis:hget(hash, msg.from.id)
-      if not value then
-         if msg.from.id == tonumber(makan) then 
-            text = text..'سلام بابایی جونم (^_^)\n\n'
-          elseif is_admin2(msg.from.id) then
-            text = text..'سلام \n\n'
-           elseif is_owner2(msg.from.id, msg.to.id) then
-            text = text..'سلام \n\n'
-          elseif is_momod2(msg.from.id, msg.to.id) then
-            text = text..'سلام \n\n'
-      else
-            text = text..'سلام\n\n'
-      end
- return text
-     
- end
- 
- return {
-   patterns = {
- 
- 
- "^[Ss]lm$",
- "^سلام$",
- "^salam",
- 
-   }, 
-   run = run 
- }
+function run(msg, matches) 
+      local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..msg.from.id
+       local  file = download_to_file(url,'emoji.webp') 
+         send_document(get_receiver(msg), file, ok_cb, false) 
+
+end 
+
+return { 
+  description = "دریافت ایدی به صورت استیکر", 
+  usage = { 
+    "پلاگین فان!", 
+  }, 
+  patterns = { 
+   "^myid$", 
+  }, 
+  run = run 
+} 
