870f4862ac21167459e3edcb2ad77e3d4b7bf34b
MoonTeamDev <makan.j130130@gmail.com>
2016-11-10 17:32:05 +0330

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,12 +1,12 @@
-local function toaudio(msg, success, result) 
+local function tovoice(msg, success, result) 
   local receiver = get_receiver(msg) 
   if success then 
-    local file = './data/toaudio/'..msg.from.id..'.ogg' 
+    local file = './data/tovoice/'..msg.from.id..'.ogg' 
     print('File downloaded to:', result) 
     os.rename(result, file) 
     print('File moved to:', file) 
     send_audio(get_receiver(msg), file, ok_cb, false) 
-    redis:del("video:audio") 
+    redis:del("video:voice") 
   else 
     print('Error downloading: '..msg.id) 
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
@@ -17,20 +17,20 @@ local function run(msg,matches)
   local receiver = get_receiver(msg) 
     local group = msg.to.id 
     if msg.reply_id then 
-       if msg.to.type == 'video' and redis:get("video:audio") then 
+       if msg.to.type == 'video' and redis:get("video:voice") then 
         if redis:set("video:audio", "waiting") then 
         end 
        end 
-      if matches[1]:lower() == "audio"  then 
-     redis:get("video:audio") 
+      if matches[1]:lower() == "voice"  then 
+     redis:get("video:voice") 
     send_large_msg(receiver, '', ok_cb, false) 
-        load_audio(msg.reply_id, toaudio, msg) 
+        load_voice(msg.reply_id, tovoice, msg) 
     end 
 end   
 end 
 return { 
 patterns = { 
-   "^[#!/](audio)$",
+   "^[#!/](voice)$",
    "%[(audio)%]", 
  }, 
 run = run, 
