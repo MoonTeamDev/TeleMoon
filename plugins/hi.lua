5e255d961159e89df135ba8afe42fc9e1760a322
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 01:26:20 +0330

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -1,34 +1,22 @@
-local function run(msg, matches)
- local makan = 90285047
-   local hash = 'rank:variables'
+function run(msg, matches)
+        local hash = 'rank:variables'
    local text = ''
      local value = redis:hget(hash, msg.from.id)
       if not value then
-         if msg.from.id == tonumber(makan) then 
-            text = text..'Hi dady (^_^)\n\n'
-          elseif is_admin2(msg.from.id) then
-            text = text..'Hi admin\n\n'
-           elseif is_owner2(msg.from.id, msg.to.id) then
-            text = text..'Hi owner\n\n'
-          elseif is_momod2(msg.from.id, msg.to.id) then
-            text = text..'Hi moderator\n\n'
-        else
-            text = text..'Hi '..value..'  \n\n'
-        else
-            text = text..'Hi chos user\n\n'
-      end
- return text
-     
- end
- 
- return {
-   patterns = {
- 
- 
- "^[Ss]lm$",
- "^سلام$",
- "^salam",
- 
-   }, 
-   run = run 
- }
+  local text ="سلام  "..msg.from.print_name
+    return text
+  end
+ else 
+  text = text..'سلام '..value..'  \n\n'
+
+return {
+  description = "Help owner.  ", 
+  usage = {
+    "ownerhelp: Show help for owners.",
+  },
+  patterns = {
+    "^([Ss]lm)$",
+  "^(سلام)$",
+  }, 
+  run = run,
+}
