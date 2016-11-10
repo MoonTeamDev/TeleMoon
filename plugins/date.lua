b89e327f102adff7d87a609a45cd8766d97da220
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 03:07:51 +0330

    Update date.lua

--- plugins/date.lua
+++ plugins/date.lua
@@ -1,34 +1,11 @@
--- by @MoonsTeam
-local text1 = { 
-"√", 
-"#", 
-"=>", 
-"<>", 
-">>", 
-} 
-local text2 = { 
-"√", 
-"#", 
-"=>", 
-"<>", 
-">>", 
-} 
-local function mrnitro(msg, matches) 
-local url , res = http.request('http://api.gpmod.ir/time/') 
-if res ~= 200 then return "!Error!" end 
-local jdat = json:decode(url) 
-local fatime = jdat.FAtime 
-local entime = jdat.ENtime 
-local fadate = jdat.FAdate 
-local endate = jdat.ENdate 
-local r1 = text1[math.random(#text1)] 
-local r2 = text2[math.random(#text2)] 
-local text = r1.."<b>FAtime:</b> "..fatime.."\n"..r2.."<b>ENtime:</b> "..entime.."\n"..r1.."<b>FAdate:</b> "..fadate.."\n"..r2.."<b>ENdate:</b> "..endate 
-return text 
-end 
-return { 
-  patterns = { 
-     "^[/!#]([Dd][Aa][Tt][Ee])$", 
-  }, 
-  run = run
-} 
+ function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "Try again!" end
+local jdat = json:decode(url)
+local text = jdat.ENdate
+return reply_msg(msg.id, text, ok_cb, false)
+end
+return {
+  patterns = {"^[/!#]([Dd][Aa][tT][Ee])$"}, 
+run = run 
+}
