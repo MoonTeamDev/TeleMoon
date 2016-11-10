1a4c60f5e7d83f8ff466b3bfbf53c2cede3f6071
MoonTeamDev <makan.j130130@gmail.com>
2016-11-10 17:34:19 +0330

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,4 +1,4 @@
-local function tovoice(msg, success, result) 
+local function toaudio(msg, success, result) 
   local receiver = get_receiver(msg) 
   if success then 
     local file = './data/tovoice/'..msg.from.id..'.ogg' 
@@ -6,7 +6,7 @@ local function tovoice(msg, success, result)
     os.rename(result, file) 
     print('File moved to:', file) 
     send_audio(get_receiver(msg), file, ok_cb, false) 
-    redis:del("video:voice") 
+    redis:del("video:audio") 
   else 
     print('Error downloading: '..msg.id) 
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
@@ -17,14 +17,14 @@ local function run(msg,matches)
   local receiver = get_receiver(msg) 
     local group = msg.to.id 
     if msg.reply_id then 
-       if msg.to.type == 'video' and redis:get("video:voice") then 
+       if msg.to.type == 'video' and redis:get("video:audio") then 
         if redis:set("video:audio", "waiting") then 
         end 
        end 
       if matches[1]:lower() == "voice"  then 
-     redis:get("video:voice") 
+     redis:get("video:audio") 
     send_large_msg(receiver, '', ok_cb, false) 
-        load_voice(msg.reply_id, tovoice, msg) 
+        load_audio(msg.reply_id, tovoice, msg) 
     end 
 end   
 end 
@@ -37,5 +37,3 @@ run = run,
 } 
 
 -- by @MoonsTeam
-
--- در صورت نیاز به هرکدام از پلاگین ها دو خط پشت پترن آنها را بردارید
