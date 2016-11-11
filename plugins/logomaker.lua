298ebf2f1f4952bcf82aa632a57840f235840d40
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 18:46:26 +0330

    Update logomaker.lua

--- plugins/logomaker.lua
+++ plugins/logomaker.lua
@@ -1,6 +1,6 @@
 function run(msg, matches)
   local text = matches[1]
-  local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..text.."
+  local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..text
    local file = download_to_file(url,'iteam.jpg')
    send_photo2(get_receiver(msg), file, "By @MoonsTeam", rmtmp_cb, cb_extra)
 
