ba9677ea36526a60eef4441e40073772b75944fd
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 22:41:36 +0330

    Update logomaker.lua

--- plugins/logomaker.lua
+++ plugins/logomaker.lua
@@ -1,5 +1,6 @@
 function run(msg, matches)
-  local text = matches[2]
+  if matches[1] = 'logo' then
+local text = matches[2]
   local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..text
    local file = download_to_file(url,'emoji.jpg')
    send_photo2(get_receiver(msg), file, "By @MoonsTeam", rmtmp_cb, cb_extra)
