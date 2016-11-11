30641da504e338adc0fadfaf5e48af1138fb5c47
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 04:33:18 +0330

    Create wlc-rank.lua

---
+++ plugins/wlc-rank.lua
@@ -0,0 +1,28 @@
+local function run(msg, matches)
+ local makan = 90285047 
+  if msg.action.type == "chat_add_user_link" then
+   local hash = 'rank:variables'
+   local text = ''
+     local value = redis:hget(hash, msg.from.id)
+      if not value then
+         if msg.from.id == tonumber(makan) then 
+            text = text..'Welcome Dady (^_^) \n\n'
+          elseif is_admin2(msg.from.id) then
+            text = text..'Welcome Admin😝 \n\n'
+          elseif is_owner2(msg.from.id, msg.to.id) then
+            text = text..'Welcome Group Owner😜 \n\n'
+          elseif is_momod2(msg.from.id, msg.to.id) then
+            text = text..'Welcome Group Moderator😛 \n\n'
+       else
+        text = text..'Hi '..value.. "\nwelcome to " ..string.gsub(msg.to.print_name, "_", " ").."\n\n"
+      end
+ return text
+  end    
+ end
+ return {
+   patterns = {
+     "^!!tgservice (chat_add_user_link)$",
+ 
+   }, 
+   run = run 
+}
