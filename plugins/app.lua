43bb6e683c5722b6aa66211375144554eec6446c
MoonTeamDev <makan.j130130@gmail.com>
2016-11-11 14:49:56 +0330

    Update app.lua

--- plugins/app.lua
+++ plugins/app.lua
@@ -8,7 +8,7 @@ local function run(msg, matches)
   
     local Store = Store.."\napkmirror :\nhttp://www.apkmirror.com/?s="..URL.escape(matches[1]).."&post_type=app_release&searchtype=apk"
   
-    local Store = Store..'\n\n\nApple Store :\n\nappsodo\nhttp://www.appsodo.com/search_'..URL.escape(matches[1]).."_1"
+    local Store = Store..'\n\n\nApple Store :\n\nappsodo\nhttp://www.appsodo.com/search_'..URL.escape(matches[1]).."_1"
    
     return Store
 end
