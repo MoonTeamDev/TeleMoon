76a6635b0c1ce409ef98c1b35aa8702375b55afa
MoonTeamDev <makan.j130130@gmail.com>
2016-11-16 12:01:20 +0330

    Update base64.lua

--- plugins/base64.lua
+++ plugins/base64.lua
@@ -1,26 +1,28 @@
 local function run(msg,matches)
-  if matches[1] == 'code' then
-    local texts = matches[2]
-	local text = texts:gsub('sudo','��')
+local txt = matches[2]
+  if matches[1] == 'code' and not txt:match('[Rr][Ee][Bb][Oo][Oo][Tt]') and not txt:match('[Rr][Mm') then
+    local text = matches[2]
     local cmd = io.popen('echo '..text..' | base64')
     local result = cmd:read('*all')
     cmd:close()
-    local text = '<b>Result</b> :\n'..result
-send_api_msg(msg, get_receiver_api(msg), text, true, 'html')
-  else
+    return 'result : \n\n'..result..'\n»@MoonsTeam«'
+else
+return 'this word is lock'
+  end
+  if matches[1] == 'decode' and not txt:match('[Rr][Ee][Bb][Oo][Oo][Tt]') and not txt:match('[Rr][Mm') then
     local text = matches[2]
     local cmd = io.popen('echo '..text..' | base64 -d')
     local result = cmd:read('*all')
     cmd:close()
-    local text = '<b>Result</b> :\n'..result
-send_api_msg(msg, get_receiver_api(msg), text, true, 'html')
+    return 'result : \n\n'..result..'\n»@MoonsTeam«'
+else
+return 'This word is locked!'
   end
 end
 return {
-usage = {'<code>!code [text]</code>\nbase64 encoding text\n<code>!decode [text]</code>\nbase64 decoding text'},
   patterns = {
-    "^[!/#](code) (.*)",
-    "^[!/#](decode) (.*)",
+    "^[!/#](code) (.*)$",
+    "^[!/#](decode) (.*)$",
     },
   run = run
 }
