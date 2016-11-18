f1fc9acfcc2bdaac5a55683fdf352604ac9dfbaa
MoonTeamDev <makan.j130130@gmail.com>
2016-11-18 14:04:39 +0330

    Update font.lua

--- plugins/font.lua
+++ plugins/font.lua
@@ -1,7 +1,4 @@
 local function run(msg, matches)
-	if not is_owner(msg) then
-		return "فقط برای صاحب گروه" 
-	end
 	if #matches < 2 then
 		return "بعد از این دستور، با قید یک فاصله کلمه یا جمله ی مورد نظر را جهت زیبا نویسی وارد کنید"
 	end
