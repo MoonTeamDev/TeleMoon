848ede1bd8957a32d119eee6f21b25883afd525f
MoonTeamDev <makan.j130130@gmail.com>
2016-11-18 23:51:14 +0330

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -15,12 +15,6 @@ if matches[1] == 'remmsg' and is_sudo(msg) then
             return "Error!"
             end
             get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
-        else
-                         return "Only for supergroup"
-        end
-else
-return "Only for sudo!"
-end
 elseif matches[1] == 'remmsg' and is_owner(msg) then
             if msg.to.type == 'channel' then
             if tonumber(matches[2]) > 999 or tonumber(matches[2]) < 1 then
