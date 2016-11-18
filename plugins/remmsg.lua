c1d69cd6292a6a5adee9d9d40574c9f3d76e5996
MoonTeamDev <makan.j130130@gmail.com>
2016-11-18 21:59:14 +0330

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -21,7 +21,7 @@ end
 local function run(msg, matches)
 if matches[1] == 'remmsg' and is_owner(msg) then
             if msg.to.type == 'channel' then
-            if tonumber(matches[2]) > 9999 or tonumber(matches[2]) < 1 then
+            if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
             return "Error!"
             end
             get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
