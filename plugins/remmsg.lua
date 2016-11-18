37ea0389fac0bb39654b4c4cf2b89b9a7b71f067
MoonTeamDev <makan.j130130@gmail.com>
2016-11-18 22:14:18 +0330

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -21,6 +21,32 @@ if matches[1] == 'remmsg' and is_sudo(msg) then
 else
 return "Only for mods!"
 end
+local function run(msg, matches)
+if matches[1] == 'remmsg' and is_owner(msg) then
+            if msg.to.type == 'channel' then
+            if tonumber(matches[2]) > 999 or tonumber(matches[2]) < 1 then
+            return "Error!"
+            end
+            get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+        else
+                         return "Only for supergroup"
+        end
+else
+return "Only for owner!"
+end
+local function run(msg, matches)
+if matches[1] == 'remmsg' and is_momod(msg) then
+            if msg.to.type == 'channel' then
+            if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
+            return "Error!"
+            end
+            get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+        else
+                         return "Only for supergroup"
+        end
+else
+return "Only for mods!"
+end
 end
 return {
     patterns = {
