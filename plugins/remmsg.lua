f2fff79815f6d225f69e77e8668ff005e8613d80
MoonTeamDev <makan.j130130@gmail.com>
2016-11-18 21:58:48 +0330

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -9,6 +9,16 @@ send_msg(extra.chatid, 'Done!', ok_cb, false)
 end
 end
 local function run(msg, matches)
+if matches[1] == 'remmsg' and is_sudo(msg) then
+            if msg.to.type == 'channel' then
+            if tonumber(matches[2]) > 9999 or tonumber(matches[2]) < 1 then
+            return "Error!"
+            end
+            get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+        else
+                         return "Only for supergroup"
+end
+local function run(msg, matches)
 if matches[1] == 'remmsg' and is_owner(msg) then
             if msg.to.type == 'channel' then
             if tonumber(matches[2]) > 9999 or tonumber(matches[2]) < 1 then
