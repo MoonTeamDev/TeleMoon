a4926613ad31e19bc14e4c14a1205a2467b2d779
MoonTeamDev <makan.j130130@gmail.com>
2016-11-21 01:06:30 +0330

    Update and rename inrealmsuper.lua to logomaker.lua

---
+++ plugins/logomaker.lua
@@ -0,0 +1,19 @@
+ function run(msg, matches) 
+      local text1 = matches[1] 
+     local text2 = matches[2] 
+      local url = "http://api.roonx.com/photo/pic4.php?text="..text1
+       local  file = download_to_file(url,'emoji.webp') 
+         send_document(get_receiver(msg), file, ok_cb, false) 
+
+end 
+
+return { 
+  description = "تبدیل متن به لوگو", 
+  usage = { 
+    "/logo text: ساخت لوگو", 
+  }, 
+  patterns = { 
+   "^[/!#][Ll]ogo (.+)$", 
+  }, 
+  run = run 
+} 
