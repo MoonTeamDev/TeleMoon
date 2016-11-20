3455452fa3db038a9bcd3c329998208c43fc53d2
MoonTeamDev <makan.j130130@gmail.com>
2016-11-20 11:40:40 +0330

    Create setchannel.lua

---
+++ plugins/setchannel.lua
@@ -0,0 +1,40 @@
+ local function run(msg, matches) 
+if matches[1] == "setchannel" then 
+if not is_admin1(msg) then 
+return 'You,re not admin' 
+end 
+local channel = matches[2] 
+redis:set('bot:channel',channel) 
+return 'Channel has been seted' 
+end 
+if matches[1] == 'channel' then 
+if not is_momod(msg) then 
+return 
+end 
+    local hash = ('bot:channel') 
+    local channel = redis:get(hash) 
+    if not channel then 
+    return 'Channel is not seted' 
+    else 
+    reply_msg(msg.id, channel, ok_cb, false) 
+    end 
+    end 
+if matches[1] == 'delchannel' then 
+if not is_admin1(msg) then 
+return 'You,re not admin' 
+end 
+    local hash = ('bot:channel') 
+    redis:del(hash) 
+return 'Channel has been deleted' 
+end 
+end 
+return { 
+patterns ={ 
+"^[!#/](setchannel) (.*)$", 
+"^[!#/](channel)$", 
+"^[!#/](delchannel)$", 
+}, 
+run = run 
+} 
+-- by @Makan
+-- channel: @MoonsTeam
