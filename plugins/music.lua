39368ab01b9edee18317620eb97a8ffbd1a353d1
MoonTeamDev <makan.j130130@gmail.com>
2016-11-20 18:58:44 +0330

    Update music.lua

--- plugins/music.lua
+++ plugins/music.lua
@@ -81,8 +81,7 @@ do
     description = 'Search and get music from pleer',
     usage = '/music [track name or artist and track name]: Search and get the music',
     patterns = {
-    '^[!/#]music (.*)$',
-    '^[!/#]getmusic (.*)$'
+    '^[!/#]music (.*)$'
     },
     run = run
 }
