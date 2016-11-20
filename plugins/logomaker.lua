c445386f97e5ebe97a6bc654678587269f311917
MoonTeamDev <makan.j130130@gmail.com>
2016-11-21 01:22:46 +0330

    Update logomaker.lua

--- plugins/logomaker.lua
+++ plugins/logomaker.lua
@@ -1,8 +1,7 @@
  function run(msg, matches) 
       local text1 = matches[1] 
      local text2 = matches[2] 
-    local text3 = matches[3]
-      local url = "http://api.roonx.com/photo/pic4.php?text="..text1.."&color="..text3
+      local url = "http://api.roonx.com/photo/pic4.php?text="..text1.."&color="..text2
        local  file = download_to_file(url,'emoji.webp') 
          send_document(get_receiver(msg), file, ok_cb, false) 
 
