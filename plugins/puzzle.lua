aacb56c780fc850f6d99726fb1a5fcbc5e2931b3
MoonTeamDev <makan.j130130@gmail.com>
2016-11-21 22:18:07 +0330

    Update puzzle.lua

--- plugins/puzzle.lua
+++ plugins/puzzle.lua
@@ -1,7 +1,7 @@
 function run(msg, matches)
-if matches[1] == 'puzzle' then
-  local text = matches[2]
-  local url = "http://www.myfunstudio.com/-/p.php?t=%0AKeep%0D%0ACalm%0D%0A"..text.."%0D%0Ais%20King&bc=e31hg7&tc=tr467&cc=y566F&uc=true&ts=true&ff=PNG&w=500&ps=sq
+if matches[1] == 'pazel' then
+  local muteteam = matches[2]
+  local url = "http://www.myfunstudio.com/-/p.php?t=%0AKeep%0D%0ACalm%0D%0A"..muteteam.."%0D%0Ais%20King&bc=e31hg7&tc=tr467&cc=y566F&uc=true&ts=true&ff=PNG&w=500&ps=sq
   local ext = "love.webp"
   local cb_extra = {file_path=file}
   local receiver = get_receiver(msg)
@@ -11,7 +11,7 @@ end
    end
 return {
   patterns = {
-   "[/!#](puzzle) (.*)",
+   "[!#/](pazel) (.*)",
   },
   run = run
 }
