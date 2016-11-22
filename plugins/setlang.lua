bb7752a34bdcdd57ba0e690c01245b56e4d99a02
MoonTeamDev <makan.j130130@gmail.com>
2016-11-23 02:58:18 +0330

    Update setlang.lua

--- plugins/setlang.lua
+++ plugins/setlang.lua
@@ -2,7 +2,7 @@ do
 local function run(msg, matches, callback, extra)
 local hash = 'group:'..msg.to.id
 local group_lang = redis:hget(hash,'lang')
-if matches[1] == 'setlang' and  matches[2] == 'en' and is_owner(msg) then 
+if matches[1] == 'نشاندن زبان' and  matches[2] == 'انگلیسی' and is_owner(msg) then 
     
    redis:hdel(hash,'lang')
         return 'group lang set to : en'
@@ -10,11 +10,11 @@ end
 
 
 
-if matches[1] == 'setlang' and matches[2] == 'fa' and is_owner(msg) then
+if matches[1] == 'نشاندن زبان' and matches[2] == 'فارسی' and is_owner(msg) then
 redis:hset(hash,'lang',matches[2])
         return 'زبان گروه تنظیم شد به : FA'
 end
-if matches[1] == 'lang' then
+if matches[1] == 'زبان' then
 if group_lang then 
 return "زبان گروه شما هم اکنون بر روی فارسی قرار دارد"
 else
@@ -24,9 +24,9 @@ end
 end
 return {
   patterns = {
-    "^[!#/]([Ss][Ee][Tt][Ll][Aa][Nn][Gg]) ([Ff][Aa])$",
-  "^[!#/]([Ss][Ee][Tt][Ll][Aa][Nn][Gg]) ([Ee][Nn])$",
-  "^[!#/]([Ll][Aa][Nn][Gg])"
+    "^[!#/](نشاندن زبان) (فارسی)$",
+  "^[!#/](نشاندن زبان) (انگلیسی)$",
+  "^[!#/](زبان)"
   },
   run = run
 }
