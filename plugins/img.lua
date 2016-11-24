6eeafa9b46d98502972e688b0280a0ca1a931ade
MoonTeamDev <makan.j130130@gmail.com>
2016-11-24 18:04:05 +0330

    Update and rename gpstats.lua to img.lua

---
+++ plugins/img.lua
@@ -0,0 +1,12 @@
+local function run(msg, matches)
+local url = "https://unsplash.it/200/200/?random"
+local file = download_to_file(url)
+send_photo(get_receiver(msg),file,OK_cb,true)
+end
+end
+return {
+patterns = {
+"^[#!/]([Ii]mg)$",
+},
+run = run,
+}
