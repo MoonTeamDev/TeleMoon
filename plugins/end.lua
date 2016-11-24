e5488d9aa111787adb3e3a8cc16c120a0c1fa1c5
MoonTeamDev <makan.j130130@gmail.com>
2016-11-24 20:58:51 +0330

    Create end.lua

---
+++ plugins/end.lua
@@ -0,0 +1,23 @@
+local function run(msg, matches, callback, extra)
+if not is_sudo(msg) then
+return false
+end
+
+local hash = 'endmsg:'
+if matches[1] == 'delend' and not matches[2] then 
+   redis:hset(hash, 'endmsg', 0)
+   return 'End msg is nil :('
+end
+
+if matches[1] == 'setend' then
+redis:hset(hash, 'endmsg', matches[2])
+        return "End msg changed :D"
+end
+end
+return {
+  patterns = {
+  "^[!#/](setend) +(.*)$",
+  "^[!#/](delend)$"
+  },
+  run = run
+}
