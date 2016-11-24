943d420708b45434cd7611b43d8f8411e96a2c0a
MoonTeamDev <makan.j130130@gmail.com>
2016-11-24 21:12:37 +0330

    Update smartspam.lua

--- plugins/smartspam.lua
+++ plugins/smartspam.lua
@@ -27,12 +27,34 @@ end
     redis:del(hash)
 return 'Spam has been deleted!'
 end
+local receiver = get_receiver(msg)
+local hash = 'bot:spam'..receiver
+     if matches[1] == 'spam' and is_sudo(msg) then
+--Enable spam
+     if matches[2] == 'on' then
+    redis:del(hash)
+   return 'spam has been enabled'
+--Disable spam
+     elseif matches[2] == 'off' then
+    redis:set(hash, true)
+   return 'spam has been disabled'
+   --Spam Status
+    elseif matches[2] == 'status' then
+      if not redis:get(hash) then
+   return 'spam is enable'
+       else
+   return 'spam is disable'
+         end
+      end
+   end
 end
 return {
 patterns ={
 "^[!#/](setspam) (.*)$",
 "^[!#/](spam)$",
 "^[!#/](delspam)$",
+"^[!#/](spam) (.*)$",
 },
 run = run
 }
+--by @MoonsTeam , @Makan
