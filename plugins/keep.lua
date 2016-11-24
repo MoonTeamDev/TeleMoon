bd0c0e95a0fb5eeee38396824cc3b38940567242
MoonTeamDev <makan.j130130@gmail.com>
2016-11-24 12:59:48 +0330

    Update keep.lua

--- plugins/keep.lua
+++ plugins/keep.lua
@@ -4,7 +4,7 @@
     local text3 = matches[3]
    local text4 = matches[4]
   local text5 = matches[5]
-      local url = "http://api.roonx.com/photo/keepcalm.php?text="..text1.."&color="..text2.."&fontbg="..text3.."&fontsize="..text4.."&fontsize2="..text5..
+      local url = "http://api.roonx.com/photo/keepcalm.php?text="..text1.."&color="..text2.."&fontbg="..text3.."&fontsize="..text4.."&fontsize2="..text5
        local  file = download_to_file(url,'emoji.webp') 
          send_document(get_receiver(msg), file, ok_cb, false) 
 
