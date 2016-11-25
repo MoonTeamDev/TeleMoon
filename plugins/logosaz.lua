da4064f1c6ae73d919415e5fb65d8a5eeca9eab6
MoonTeamDev <makan.j130130@gmail.com>
2016-11-25 13:30:49 +0330

    Create logosaz.lua

---
+++ plugins/logosaz.lua
@@ -0,0 +1,22 @@
+ function run(msg, matches) 
+      local text1 = matches[1] 
+     local text2 = matches[2]
+    local text3 = matches[3]
+      local url = "http://api.roonx.com/photo/logo2.php?text="..text1.."&color="..text2
+       local  file = download_to_file(url,'emoji.webp') 
+         send_document(get_receiver(msg), file, ok_cb, false) 
+
+end 
+
+return { 
+  description = "تبدیل متن به لوگو", 
+  usage = { 
+    "/logo text: ساخت لوگو", 
+  }, 
+  patterns = { 
+   "^[/!#][Ll]ogo (.+) (.+)$", 
+  }, 
+  run = run 
+} 
+
+--by @MoonsTeam , @Makan
