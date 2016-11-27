377522a2c296522d08250424c1f309e0da9a7339
MoonTeamDev <makan.j130130@gmail.com>
2016-11-27 16:31:09 +0330

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1769,7 +1769,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				return "SuperGroup ID for " ..string.gsub(msg.to.print_name, "_", " ").. ":\n\n"..msg.to.id
+				return " ..string.gsub(msg.to.print_name, "_", " ").. ": "..msg.to.id
 			end
 		end
 		if matches[1] == 'kickme' then
