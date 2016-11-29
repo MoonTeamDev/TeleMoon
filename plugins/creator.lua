7aacda99d656ed3f0030d8d9928083dc7a58775c
MoonTeamDev <makan.j130130@gmail.com>
2016-11-29 21:58:08 +0330

    Create creator.lua

---
+++ plugins/creator.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return [[<b>This bot created by</b> @Makan]
+end
+return {
+  description = "creator",
+  usage = "creator",
+  patterns = {
+    "^[!/#]creator$"
+  }, 
+  run = run 
+}
+end
