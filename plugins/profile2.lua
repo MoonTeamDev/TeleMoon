f81fbefeadb30d3ae08cd153c7a40c85faefc9a1
MoonTeamDev <makan.j130130@gmail.com>
2016-12-02 20:06:57 +0330

    Update profile2.lua

--- plugins/profile2.lua
+++ plugins/profile2.lua
@@ -3,7 +3,7 @@ if matches[1] == 'profile>' then
   local moonsteam = matches[2]
   local moonsteam2 = matches[3]
   local moonsteam3 = matches[4]
-  local url = "http://api.roonx.com/photo/shakh/pic.php?text="..moonsteam.."&color="..moonsteam2.."&size="..moonsteam3
+  local url = "http://api.roonx.com/photo/shakh/pic2.php?text="..moonsteam.."&color="..moonsteam2.."&size="..moonsteam3
   local ext = "emoji.webp"
   local cb_extra = {file_path=file}
   local receiver = get_receiver(msg)
